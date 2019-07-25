030c2fe86671302c101b2a2989ea77efa4628c88
Anan Roderick <thamarnan@gmail.com>
2019-07-25 10:17:53 -0700

    Update checksite.cmd

--- checksite.cmd
+++ checksite.cmd
@@ -1,9 +1,16 @@
+REM CheckSite.cmd wrapper with powershell
+REM Return http status and geolocation from ipinfo
+REM alert email or webhook
+
+
 @PowerShell -ExecutionPolicy Bypass -Command Invoke-Expression $('$args=@(^&{$args} %*);'+[String]::Join(';',(Get-Content '%~f0') -notmatch '^^@PowerShell.*EOF$')) & goto :EOF
 
-$SITE = "http://testsite.com"
+
+$SITE = "http://www.google.com"
+
+
 $myLocation = Invoke-RestMethod http://ipinfo.io/json | Select -exp region
 $nl = [Environment]::NewLine
-
 try{
     $request = $null
     $request = Invoke-WebRequest -Uri $SITE
@@ -12,21 +19,25 @@ try{
     }  
     $StatusCode = [int] $request.StatusCode;
     $StatusDescription = $request.StatusDescription;
-    Write-Host $StatusCode  $SITE $myLocation 
+    Write-Host $StatusCode $SITE $myLocation 
 
 
 If ($StatusCode  -eq 200){
     Write-Host "Site is OK!"
 } Else {
-    	Write-Host "The Site may be down, please check!" 
-	$EmailFrom = "@gmail.com"
-	$EmailTo = "@gmail.com"
+    Write-Host "The Site may be down, please check!" 
+	$EmailFrom = "fromEmail@gmail.com"
+	$EmailTo = "toEmail@gmail.com"
 	$Subject = "Alert: Status " + $StatusCode +" on " + $SITE
 	$Body = "Status: " + $StatusCode + " on " + $SITE + $nl + "Source Location: " + $myLocation
 	$SMTPServer = "smtp.gmail.com"
 	$SMTPClient = New-Object Net.Mail.SmtpClient($SmtpServer, 587)
 	$SMTPClient.EnableSsl = $true
-	$SMTPClient.Credentials = New-Object System.Net.NetworkCredential("a@gmail.com", "password");
-	$SMTPClient.Send($EmailFrom, $EmailTo, $Subject, $Body)
+	$SMTPClient.Credentials = New-Object System.Net.NetworkCredential("fromEmail@gmail.com", "emailPassword");
+	<#$SMTPClient.Send($EmailFrom, $EmailTo, $Subject, $Body)#>
+
+	$ReqURI = 'http://webhookurl'
+	$Jsonbody= @{ 'value1' = $SITE ; 'value2' = 'Alert: Problem Accessing site'; } | ConvertTo-Json
+	$Response = Invoke-RestMethod -Method Post -ContentType 'application/json' -Uri $ReqURI -Body $Jsonbody
 }
 
